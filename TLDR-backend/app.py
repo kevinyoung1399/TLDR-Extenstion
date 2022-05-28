f55106160bf6c3ed454036e30d119fddef775660
kevinyoung1399 <iamkevinyoung@gmail.com>
2022-05-28 20:44:36 +0100

    Refactor with Pylint

--- TLDR-backend/app.py
+++ TLDR-backend/app.py
@@ -1,18 +1,26 @@
+"""Flask, a web framework used for processing the POST requests. """
 from flask import Flask, jsonify, request, abort
 from flask_cors import CORS
-from summarizers.Abstractive import Abstractive#
-from summarizers.Extractive import Extractive
+from summarizers.abstractive import Abstractive
+from summarizers.extractive import Extractive
 
-from flask import Flask
+
 app = Flask(__name__)
 CORS(app)
 
 @app.route("/")
 def home():
+    """
+    GET request to verify Flask server is running.
+    """
     return "Hello, Flask!"
 
+
 @app.route("/extractiveSummarize", methods=['POST'], endpoint='extractive')
-def handleExtractive():
+def handle_extractive():
+    """
+    Process extractiveSummarize POST request.
+    """
     if not request.json or not 'paragraphs' in request.json:
         abort(400, 'No data found to summarize.')
     ext = Extractive(request.json['paragraphs'])
@@ -20,10 +28,14 @@ def handleExtractive():
     summarizations = ext.extract()
     return jsonify({'summarizations': summarizations})
 
+
 @app.route("/abstractiveSummarize", methods=['POST'], endpoint='abstractive')
-def handleAbstractive():
+def handle_abstractive():
+    """
+    Process abstractiveSummarize POST request.
+    """
     if not request.json or not 'paragraphs' in request.json:
         abort(400, 'No data found to summarize.')
-    abs = Abstractive(request.json['paragraphs'])
-    summarizations = abs.collect_summarizations()
+    abst = Abstractive(request.json['paragraphs'])
+    summarizations = abst.collect_summarizations()
     return jsonify({'summarizations': summarizations})
--- TLDR-backend/summarizers/Abstractive.py
+++ TLDR-backend/summarizers/Abstractive.py
@@ -1,16 +1,32 @@
+"""Module for downstream tasks, required to run the BART summarizer"""
 from transformers import pipeline
 
 class Abstractive:
+    """
+    A class to process abstractive summarizations using BART
+    ...
+    Attributes
+    ----------
+    input paragraphs : str
+        paragraphs to summarize.
+    summarizations : list
+        sentences that are output from the BertSum, used for the summarization.
+    """
+
+
     def __init__(self, input_paragraphs):
         """
         """
         self._input_paragraphs = input_paragraphs
         self._summarizations = []
 
+
     def collect_summarizations(self):
         """
-        Gets most important 3
-        :return: list : summarizations
+        Creates a list of all the summarizations.
+        Returns
+        -------
+        summarizations : str
         """
         summarizations = []
         for paragraph in self._input_paragraphs:
@@ -18,12 +34,16 @@ class Abstractive:
         self._summarizations = summarizations
         return summarizations
 
+
     def abstract(self, paragraph):
         """
-        Gets most important with BERTSum
-        :return: list : summarizations
+        Creates the summarization using the Huggingface BART model.
+        Returns
+        -------
+        summary_text : str
         """
         summarizer = pipeline("summarization", model="facebook/bart-large-cnn")
         # summarizer = pipeline("summarization", model="./models/bart-large-cnn")
-        summary_text = summarizer(paragraph, max_length=130, min_length=30, do_sample=False)[0]['summary_text']
+        summary_text = summarizer(paragraph, max_length=130, min_length=30, do_sample=False)
+        summary_text = summary_text[0]['summary_text']
         return summary_text
